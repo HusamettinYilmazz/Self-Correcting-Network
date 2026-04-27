ba3b4ce756e4e63e05a017e08b328f4774f90066
Husam <husamahmed616@gmail.com>
2026-04-28 00:16:49 +0300

    train primary model 3rd stage

---
+++ train/train_3rd_stage.py
@@ -0,0 +1,48 @@
+from itertools import cycle
+import torch
+
+
+def train_model_epoch(epoch, data_loaders, device, models,
+                       optimizers, loss_func, scaler, logger, lambda_u=0.5):
+    
+    total_loss = 0.0
+    
+    models["primary"].train()
+    models["correcting"].eval()
+    models["ancillary"].eval()
+    for batch_idx, ((f_imgs, f_bbox, f_masks), (w_imgs, w_bbox)) in \
+        enumerate(zip(data_loaders["f_loader"], cycle(data_loaders["w_loader"]))):
+        f_imgs, f_masks = f_imgs.to(device), f_masks.to(device).long()
+        w_imgs, w_bbox = w_imgs.to(device), w_bbox.to(device)
+
+        f_primary_outputs = models["primary"](f_imgs)
+        w_primary_outputs = models["primary"](w_imgs)
+
+        with torch.no_grad():
+                ancillary_outputs = models["ancillary"](w_imgs, w_bbox)
+                correcting_logits = models["correcting"](
+                    w_primary_outputs.detach(), 
+                    ancillary_outputs.detach()
+                )
+
+        primary_loss = loss_func(f_primary_outputs, f_masks)
+        unsup_loss = unsupervised_loss(w_primary_outputs, correcting_logits)
+
+        loss = primary_loss + lambda_u * unsup_loss
+        optimizers["primary"].zero_grad()
+        loss.backward()
+        optimizers["primary"].step()
+        
+        total_loss += loss.item()
+
+        if batch_idx % 10 == 0:
+            logger.info(f"TRAIN PRIMARY MODEL: Epoch:{epoch} \
+                        at Batch:{batch_idx}/{len(data_loaders['f_loader'])} \
+                        Primary Loss:{primary_loss.item():.3f} |\
+                        Unsupervised Loss:{unsup_loss.item():.3f} &\
+                        Combined Loss:{loss.item():.3f}")
+    
+    avg_loss = total_loss/ len(data_loaders["f_loader"])
+    logger.info(f"PRIMARY MODEL Epoch:{epoch} average train Loss:{avg_loss:.3f}")
+    
+    return avg_loss
