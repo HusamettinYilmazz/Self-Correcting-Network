ff2aba914096c109991ebfe7a73ccad543ea88b3
Husam <husamahmed616@gmail.com>
2026-04-30 20:48:35 +0300

    Fix typo in logging

--- train/train_1st_stage.py
+++ train/train_1st_stage.py
@@ -1,8 +1,11 @@
+import torch
+from utils.eval import compute_confusion_matrix, compute_iou_per_class
+from utils.eval import compute_per_class_accuracy
 
 def train_ancillary_model_epoch(epoch, data_loader, device, models, optimizers, loss_func, scaler, logger):
     total_loss = 0.0
     models["ancillary"].train()
-    for batch_idx, (imgs, bboxs), masks in enumerate(data_loader):
+    for batch_idx, (imgs, bboxs, masks) in enumerate(data_loader):
         imgs, bboxs, masks = imgs.to(device), bboxs.to(device), masks.to(device).long()
         optimizers["ancillary"].zero_grad()
         outputs = models["ancillary"](imgs, bboxs)
@@ -14,8 +17,11 @@ def train_ancillary_model_epoch(epoch, data_loader, device, models, optimizers, 
         total_loss += loss.item()
 
         if batch_idx % 10 == 0:
-            logger.info(f"TRAIN: Epoch:{epoch} at Batch:{batch_idx}/{len(data_loader)} Loss:{loss.item():.3f}")
+            logger.info(f"TRAIN: Epoch:{epoch} at \
+                        Batch:{batch_idx}/{len(data_loader)} \
+                        Loss:{loss.item():.3f}")
     
     avg_loss = total_loss/ len(data_loader)
-    logger.info(f"ANCILLARY MODEL Epoch:{epoch} average train Loss:{avg_loss:.3f}")
+    logger.info(f"ANCILLARY MODEL Epoch:{epoch} \
+                average train Loss:{avg_loss:.3f}")
     return avg_loss
