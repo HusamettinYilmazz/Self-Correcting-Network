c4ff714ff53b5f79e2d70102b76a989a9a5d9dcb
Husam <husamahmed616@gmail.com>
2026-04-30 18:43:38 +0300

    Add no self correction network to ancillary model

---
+++ models/self_correcting_model/networks/no_self_correction.py
@@ -0,0 +1,7 @@
+import torch
+from ..self_correction_interface import SelfCorrectionModule
+
+class NoCorrectionModule(SelfCorrectionModule):
+    
+    def forward(self, primary_logits: torch.Tensor, ancillary_logits: torch.Tensor) -> torch.Tensor:
+        return torch.softmax(ancillary_logits, dim=1)
