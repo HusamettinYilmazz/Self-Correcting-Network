13bf633cbb39cfc71ab68f926098fe7df9c46421
Husam <husamahmed616@gmail.com>
2026-04-30 23:11:05 +0300

    Add stage 2 validation [valdiate correction model]

--- train/train_2nd_stage.py
+++ train/train_2nd_stage.py
@@ -51,3 +51,47 @@ def train_correction_model_epoch(epoch, data_loader, device, models, optimizers,
     logger.info(f"CORRECTING NETWORK Epoch:{epoch} average train Loss:{correcting_avg_loss:.3f}")
     logger.info(f"Epoch:{epoch} average train Loss:{(primary_avg_loss+correcting_avg_loss)/2:.3f}")
     return primary_avg_loss, correcting_avg_loss
+
+def validate_correction_model(epoch, data_loader, device, model, loss_func, class_names, logger, save_dir=None):
+    model.eval()
+
+    total_loss = 0.0
+    total_cm = None
+
+    with torch.no_grad():
+        for imgs, bboxes, masks in data_loader:
+            imgs = imgs.to(device)
+            masks = masks.to(device).long()
+
+            outputs = model(imgs)
+            loss = loss_func(outputs, masks)
+
+            total_loss += loss.item()
+
+            preds = outputs.argmax(dim=1)
+
+            cm = compute_confusion_matrix(
+                masks,
+                preds,
+                class_names,
+                ignore_index=255
+            )
+
+            total_cm = cm if total_cm is None else total_cm + cm
+
+    iou_per_class = compute_iou_per_class(total_cm)
+    acc_per_class = compute_per_class_accuracy(total_cm)
+
+    metrics = {
+        "avg_loss": total_loss / len(data_loader),
+        "iou_per_class": iou_per_class,
+        "acc_per_class": acc_per_class,
+    }
+
+    logger.info(f"Epoch: {epoch} | Stage 2 validation")
+    logger.info(metrics)
+
+    if save_dir is not None:
+        plot_confusion_matrix(total_cm, class_names, save_path=save_dir)
+
+    return metrics
