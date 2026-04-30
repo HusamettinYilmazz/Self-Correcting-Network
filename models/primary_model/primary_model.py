e11477e34ebf977471c4177126a197089a5a3045
Husam <husamahmed616@gmail.com>
2026-04-30 18:35:11 +0300

    Add primary model

---
+++ models/primary_model/primary_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+from components import SegmentationEncoder
+from components import SegmentationDecoder
+
+class PrimarySegmentationModel(nn.Module):
+    def __init__(self, num_classes: int, backbone: str = "resnet101", pretrained: bool = True):
+        super().__init__()
+        self.encoder = SegmentationEncoder(backbone, pretrained)
+        self.decoder = SegmentationDecoder(self.encoder.low_level_channels, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        low, high = self.encoder(x)
+        return self.decoder(low, high, target_size=x.shape[-2:])
