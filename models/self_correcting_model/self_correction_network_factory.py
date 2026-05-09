d76aceba4e730d973f143dfd016f20fc4dd72fd8
Husam <husamahmed616@gmail.com>
2026-05-09 15:48:08 +0300

    Fix typo in Enums using

--- models/self_correcting_model/self_correction_network_factory.py
+++ models/self_correcting_model/self_correction_network_factory.py
@@ -7,16 +7,16 @@ class SelfCorrectingNetwrokFactory:
     def __init__(self):
         ...
 
-    def build_correction_module(variant: str, num_classes: int, 
+    def build_correction_module(self, variant: str, num_classes: int, 
         alpha_start: float = 30.0, alpha_end: float = 0.5, ) -> SelfCorrectionModule:
         
         
         variant = variant.lower()
-        if variant == SelfCorrectingEnums.NO_CORRECTION:
+        if variant == SelfCorrectingEnums.NO_CORRECTION.value:
             return NoCorrectionModule()
-        elif variant == SelfCorrectingEnums.Linear_CORRECTION:
+        elif variant == SelfCorrectingEnums.Linear_CORRECTION.value:
             return LinearCorrectionModule(alpha_start=alpha_start, alpha_end=alpha_end)
-        elif variant == SelfCorrectingEnums.CONV_CORRECTION:
+        elif variant == SelfCorrectingEnums.CONV_CORRECTION.value:
             return ConvCorrectionModule(num_classes=num_classes)
         
         else:
