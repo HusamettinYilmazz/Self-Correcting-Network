761c5ff25be5387fb38968fb6e670f7ed24f49b1
Husam <husamahmed616@gmail.com>
2026-05-10 14:35:18 +0300

    Add dice loss to ancillary model

--- train/train_1st_stage.py
+++ train/train_1st_stage.py
@@ -2,7 +2,7 @@ import torch
 from utils.eval import compute_confusion_matrix, plot_confusion_matrix
 from utils.eval import compute_per_class_accuracy,  compute_iou_per_class
 
-def train_ancillary_model_epoch(epoch, data_loader, device, models, optimizers, loss_func, scaler, logger):
+def train_ancillary_model_epoch(epoch, data_loader, device, models, optimizers, loss_funcs, scaler, logger):
     total_loss = 0.0
     models["ancillary"].train()
     for batch_idx, (imgs, bboxs, masks) in enumerate(data_loader):
@@ -10,24 +10,26 @@ def train_ancillary_model_epoch(epoch, data_loader, device, models, optimizers, 
         optimizers["ancillary"].zero_grad()
         outputs = models["ancillary"](imgs, bboxs)
 
-        loss = loss_func(outputs, masks)
+        ce_loss = loss_funcs["ce_loss"](outputs, masks)
+        dice_loss = loss_funcs["dice_loss"](outputs, masks)
+        loss = ce_loss + dice_loss
 
         loss.backward()
         optimizers["ancillary"].step()
         total_loss += loss.item()
 
         if batch_idx % 10 == 0:
-            logger.info(f"TRAIN: Epoch:{epoch} at \
-                        Batch:{batch_idx}/{len(data_loader)} \
-                        Loss:{loss.item():.3f}")
+            logger.info(f"TRAIN: Epoch:{epoch} at Batch:{batch_idx}/{len(data_loader)} CrossEntropy Loss:{ce_loss.item():.3f} | Dice Loss:{dice_loss.item():.3f} | Loss:{loss.item():.3f}")
     
     avg_loss = total_loss/ len(data_loader)
-    logger.info(f"ANCILLARY MODEL Epoch:{epoch} \
-                average train Loss:{avg_loss:.3f}")
+    logger.info(f"ANCILLARY MODEL Epoch:{epoch} average train Loss:{avg_loss:.3f}")
+    
     return avg_loss
 
-def validate_ancillary_model(epoch, data_loader, device, models, loss_func, class_names, logger, save_dir=None):
+def validate_ancillary_model(epoch, data_loader, device, models, loss_funcs, class_names, logger, save_dir=None):
     total_loss = 0.0
+    total_ce_loss = 0.0
+    total_dice_loss = 0.0
     total_cm = None
 
     models["ancillary"].eval()
@@ -38,7 +40,12 @@ def validate_ancillary_model(epoch, data_loader, device, models, loss_func, clas
             masks = masks.to(device).long()
 
             preds = models["ancillary"](imgs, bboxes)
-            loss = loss_func(preds, masks)
+
+            ce_loss = loss_funcs["ce_loss"](preds, masks)
+            total_ce_loss += ce_loss.item()
+            dice_loss = loss_funcs["dice_loss"](preds, masks)
+            total_dice_loss += dice_loss.item()
+            loss = ce_loss + dice_loss
             total_loss += loss.item()
 
             cm = compute_confusion_matrix(
@@ -54,6 +61,8 @@ def validate_ancillary_model(epoch, data_loader, device, models, loss_func, clas
     acc = compute_per_class_accuracy(total_cm)
 
     metrics = {
+        "avg_ce_loss": total_ce_loss / len(data_loader),
+        "avg_dice_loss": total_dice_loss / len(data_loader),
         "avg_loss": total_loss / len(data_loader),
         "acc_per_class": acc,
         "iou_per_class": iou,
